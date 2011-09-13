c42a11a7cd7f45aaeba1d273c164b10a401767bb
eddy.castro.q7i <eddy.castro.q7i@gmail.com>
2011-09-13 04:39:10 +0000

    Ya están hechas casi todas las clases, la de Aerolinea es compartida porque tiene partes de cada uno. Ahora me voy a dedicar a trabajar solo en lo mío.

---
+++ Proyecto1/src/Aerolinea.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+Autores: 
+-Eddy Omar Castro Jauregui
+-Ana Guisela Alfaro Marroquin
+-Freddy Alejandro Chinchilla Culajay
+Carne:11032
+Seccion: 21
+Fecha:  de 2011
+Nombre del archivo: Aerolinea.java
+Breve descripcion: En esta clase se generan todos los elementos que conforman una
+aerolinea, se realiza por medio de listas.
+Utiliza métodos get() y set().
+ *******************************************************************************/
+/*import java.util.ArrayList;*/
+import java.util.List;
+
+public class Aerolinea {
+    //Definición de atributos
+    private String nombre;
+    /*private Boleto boleto[];*/
+    private List<Boleto> boleto;// = new ArrayList<String> ();   
+    
+    //Constructor
+    public Aerolinea(String n){
+        nombre = n;
+    }
+    //Parametros: no aplica
+    //Funcinalidad: obtener datos
+    //Valor de retorno: nombre
+    public String getNombre() {
+        return nombre;
+    }
+    //Parametros: variable String nombre
+    //Funcinalidad: almacenar datos
+    //Valor de retorno: no aplica
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void comprarBoleto(){
+        char a = 'P';
+        String p = "Eddy";
+        String v = "001-tx";
+        boleto.add(new Boleto(a,p,v));
+    }
+}
